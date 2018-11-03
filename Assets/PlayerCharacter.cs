69f5d74c600232ece59da2a8188c4bcd72840f93
Chris Yungmann <yungmann.chris@gmail.com>
2018-11-02 22:14:15 -0500

    we have a player!

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnGUI()
+    {
+        Vector3 direction;
+        var currentEvent = Event.current;
+        if (!currentEvent.isKey)
+        {
+            return;
+        }
+        switch (currentEvent.keyCode)
+        {
+            case KeyCode.UpArrow:
+                direction = Vector3.forward;
+                break;
+            case KeyCode.DownArrow:
+                direction = Vector3.back;
+                break;
+            case KeyCode.LeftArrow:
+                direction = Vector3.left;
+                break;
+            case KeyCode.RightArrow:
+                direction = Vector3.right;
+                break;
+            default:
+                return;
+        }
+
+        transform.Translate(direction * Time.deltaTime, Space.World);
+    }
+
+
+}

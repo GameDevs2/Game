c89d0991e1faead5889d126c884d7ff4db24288f
Daniel Baker <daniel@DESKTOP-PRP3A97.localdomain>
2018-11-11 17:20:39 -0600

    Basic functional room

---
+++ Assets/Scripts/RoomBehavior/MoveControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<PlayerCharacter>() != null){
+			GameObject room = GameObject.Find("Room");
+			room.GetComponent<Room>().setTargetRotation(new Vector3(90, 0, 0));
+		}
+	}
+}
---
+++ Assets/Scripts/RoomBehavior/Room.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	private Vector3 targetRotation = new Vector3(0,0,0);
+	private readonly float rotationTime = 3;
+	private Vector3 lastRotation;
+	private float startTime = 0;
+
+	public void setTargetRotation(Vector3 newRotation){
+		this.lastRotation = this.transform.eulerAngles;
+		this.targetRotation = newRotation;
+		this.startTime = Time.time;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		this.transform.eulerAngles = Vector3.Lerp(this.lastRotation, this.targetRotation, ((Time.time - this.startTime) / this.rotationTime));
+
+	}
+}
